bc7286ac3777bd1cf6b9ab560a8b4eb7d4a19e8f
Sadik-Sami <miasimanto@gmail.com>
2024-10-13 03:13:18 +0600

    added banner section, footer and completed home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import MyProfile from './pages/MyProfile';
 import MyAppointments from './pages/MyAppointments';
 import Appointment from './pages/Appointment';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 const App = () => {
 	return (
 		<div className='mx-4 sm:mx-[10%]'>
@@ -16,12 +17,13 @@ const App = () => {
 				<Route path='/' element={<Home />} />
 				<Route path='/doctors' element={<Doctors />} />
 				<Route path='/doctors/:speciality' element={<Doctors />} />
-				<Route path='/about' element={<About/>} />
-				<Route path='/contact' element={<Contact/>} />
-				<Route path='/my-profile' element={<MyProfile/>} />
-				<Route path='/my-appointments' element={<MyAppointments/>} />
-				<Route path='/appointments/:docId' element={<Appointment/>} />
+				<Route path='/about' element={<About />} />
+				<Route path='/contact' element={<Contact />} />
+				<Route path='/my-profile' element={<MyProfile />} />
+				<Route path='/my-appointments' element={<MyAppointments />} />
+				<Route path='/appointments/:docId' element={<Appointment />} />
 			</Routes>
+			<Footer />
 		</div>
 	);
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ const Navbar = () => {
 	return (
 		<div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
 			<img
+				onClick={() => navigate('/')}
 				className='w-44 cursor-pointer'
 				src={assets.logo}
 				alt='logo'
@@ -32,26 +33,41 @@ const Navbar = () => {
 				</NavLink>
 			</ul>
 			<div className='flex items-center gap-4'>
-        {
-          token 
-          ? <div className='flex items-center gap-2 cursor-pointer group relative'>
-            <img className='w-8 rounded-full' src={assets.profile_pic} alt="user" />
-            <img className='w-2.5' src={assets.dropdown_icon} alt="dropdown" />
-            <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
-              <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-                <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
-                <p onClick={()=>navigate('my-appointments')} className='hover:text-black cursor-pointer'>My Appointments</p>
-                <p onClick={() => setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
-              </div>
-            </div>
-          </div> 
-          : <button
-					onClick={() => navigate('/login')}
-					className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>
-					Create Account
-				</button>
-        }
-				
+				{token ? (
+					<div className='flex items-center gap-2 cursor-pointer group relative'>
+						<img
+							className='w-8 rounded-full'
+							src={assets.profile_pic}
+							alt='user'
+						/>
+						<img className='w-2.5' src={assets.dropdown_icon} alt='dropdown' />
+						<div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+							<div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+								<p
+									onClick={() => navigate('my-profile')}
+									className='hover:text-black cursor-pointer'>
+									My Profile
+								</p>
+								<p
+									onClick={() => navigate('my-appointments')}
+									className='hover:text-black cursor-pointer'>
+									My Appointments
+								</p>
+								<p
+									onClick={() => setToken(false)}
+									className='hover:text-black cursor-pointer'>
+									Logout
+								</p>
+							</div>
+						</div>
+					</div>
+				) : (
+					<button
+						onClick={() => navigate('/login')}
+						className='bg-primary text-white px-8 py-3 rounded-full font-light hidden md:block'>
+						Create Account
+					</button>
+				)}
 			</div>
 		</div>
 	);
