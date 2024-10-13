a1cb7d882fb4b4f00cf0f85979d6ce38815d5f81
Sadik-Sami <miasimanto@gmail.com>
2024-10-14 01:14:42 +0600

    about, contact page done. appointment page working

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -3,9 +3,13 @@ import { doctors } from '../assets/assets';
 
 export const AppContext = createContext();
 const AppContextProvider = (props) => {
+	const currencySymbol = '$';
 	const value = {
 		doctors,
+		currencySymbol,
 	};
-	return <AppContext.Provider value={value}>{props.children}</AppContext.Provider>;
+	return (
+		<AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+	);
 };
 export default AppContextProvider;
