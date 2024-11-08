90f47de79b9b41bdddc672cebc3deb1175aff702
Sadik-Sami <miasimanto@gmail.com>
2024-11-08 14:58:19 +0600

    backend api for getting user profile, updating user profile and implementing them on frontend

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -4,11 +4,13 @@ import axios from 'axios';
 export const AppContext = createContext();
 const AppContextProvider = (props) => {
 	const currencySymbol = '$';
+	const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
 	const [doctors, setDoctors] = useState([]);
 	const [token, setToken] = useState(
 		localStorage.getItem('token') ? localStorage.getItem('token') : false
 	);
-	const backendUrl = import.meta.env.VITE_BACKEND_URL;
+	const [userData, setUserData] = useState(false);
 	const getDoctorsData = async () => {
 		try {
 			const { data } = await axios.get(backendUrl + '/api/doctor/list');
@@ -22,15 +24,40 @@ const AppContextProvider = (props) => {
 			toast.error(error.message);
 		}
 	};
+	const loadUserProfileData = async () => {
+		try {
+			const { data } = await axios.get(backendUrl + '/api/user/get-profile', {
+				headers: { token },
+			});
+			if (data.success) {
+				setUserData(data.userData);
+			} else {
+				toast.error(data.message);
+			}
+		} catch (error) {
+			console.log(error);
+			toast.error(error.message);
+		}
+	};
 	useEffect(() => {
 		getDoctorsData();
 	}, []);
+	useEffect(() => {
+		if (token) {
+			loadUserProfileData();
+		} else{
+			setUserData(false);
+		}
+	}, [token]);
 	const value = {
 		doctors,
 		currencySymbol,
 		token,
 		setToken,
 		backendUrl,
+		userData,
+		setUserData,
+		loadUserProfileData
 	};
 	return (
 		<AppContext.Provider value={value}>{props.children}</AppContext.Provider>
